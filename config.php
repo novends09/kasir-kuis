ee7a0b6b9a4d38b68816ca8a35e3164a416a80cf
novends09 <ynirmalanovends10@gmail.com>
2021-12-08 13:52:51 +0700

    add dockerfile

--- config.php
+++ config.php
@@ -1,36 +1,12 @@
 <?php
-/*
-  | Source Code Aplikasi Penjualan Barang Kasir dengan PHP & MYSQL
-  | 
-  | @package   : pos-kasir-php
-  | @file	   : config.php ( untuk mengatur koneksi php ke database mysql )
-  | @author    : fauzan1892 / Fauzan Falah
-  | @copyright : Copyright (c) 2017-2021 Codekop.com (https://www.codekop.com)
-  | @blog      : https://www.codekop.com/read/source-code-aplikasi-penjualan-barang-kasir-dengan-php-amp-mysql-gratis.html
-  |
-  | 
-  | keterangan : untuk login aplikasi dengan username : admin dan password : 123
-  | 
-  | 
-  | 
- */
-
-date_default_timezone_set("Asia/Jakarta");
-error_reporting(0);
-
-	// sesuaikan dengan server anda
-	$host 	= 'localhost'; // host server
-	$user 	= 'root';  // username server
-	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
-	$dbname = 'db_toko'; // nama database anda
-	
-	try{
-		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
-		//echo 'sukses';
-	}catch(PDOException $e){
-		echo 'KONEKSI GAGAL' .$e -> getMessage();
-	}
-	
-	$view = 'fungsi/view/view.php'; // direktori fungsi select data
+define('DB_SERVER','172.20.0.2');
+define('DB_USER','root');
+define('DB_PASS','admin');
+define('DB_NAME','db_toko');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check Connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 ?>
-
