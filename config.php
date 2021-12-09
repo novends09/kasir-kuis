5aa2531b0bbb79325df567f50fe6bf158315add8
novends09 <ynirmalanovends10@gmail.com>
2021-12-09 13:00:50 +0700

    config

--- config.php
+++ config.php
@@ -1,27 +1,11 @@
 
 <?php
-/*
-  | Source Code Aplikasi Penjualan Barang Kasir dengan PHP & MYSQL
-  | 
-  | @package   : pos-kasir-php
-  | @file	   : config.php ( untuk mengatur koneksi php ke database mysql )
-  | @author    : fauzan1892 / Fauzan Falah
-  | @copyright : Copyright (c) 2017-2021 Codekop.com (https://www.codekop.com)
-  | @blog      : https://www.codekop.com/read/source-code-aplikasi-penjualan-barang-kasir-dengan-php-amp-mysql-gratis.html
-  |
-  | 
-  | keterangan : untuk login aplikasi dengan username : admin dan password : 123
-  | 
-  | 
-  | 
- */
-
 date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 
 	// sesuaikan dengan server anda
-	$host 	= '172.20.0.2'; // host server
-	$user 	= 'root';  // username server
+	$host 	= '10.0.0.201'; // host server
+	$user 	= 'novends';  // username server
 	$pass 	= 'Novend1*'; // password server, kalau pakai xampp kosongin saja
 	$dbname = 'db_toko'; // nama database anda
 	
